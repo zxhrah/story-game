4b1cfa906c732abd7e80a10dcb5e5b5cff0e23b2
zxhrah <153936262+zxhrah@users.noreply.github.com>
2025-04-24 23:45:11 +0100

    updateFloor
    added glowing portal for exit scene 1

---
+++ scenes/scene_4.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_entrance_2d_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		SceneTransition.change_scene("res://scenes/scene_3.tscn")
+
+
+func _on_exit_2d_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		SceneTransition.change_scene("res://scenes/gallery.tscn")
