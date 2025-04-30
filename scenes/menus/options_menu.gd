6fe64f15fb85f99860674a392c0aa455da6c3e20
zxhrah <153936262+zxhrah@users.noreply.github.com>
2025-04-30 14:19:54 +0100

    fixedUIButtons
    fixed the settings and leaderboard buttons

--- scenes/menus/options_menu.gd
+++ scenes/menus/options_menu.gd
@@ -1,28 +1,28 @@
 extends Control
 
+@onready var save_button = $AudioOptions/Save
+@onready var vbox = $AudioOptions/VBoxContainer
 
-# Called when the node enters the scene tree for the first time.
+# Preload your icon here
+var save_icon = preload("res://assets/other/settings.png")  # <-- update path to your actual icon
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	_update_button_appearance()
+	save_button.pressed.connect(_on_save_pressed)
 
 func _on_save_pressed():
-	var vbox = $AudioOptions/VBoxContainer
-	var save_button = $AudioOptions/Save
+	vbox.visible = !vbox.visible
 
-	# Toggle visibility and update text accordingly
-	if vbox.visible:
-		vbox.visible = false
-		save_button.text = "⚙️"
-		# Apply the volume setting when hiding
+	if !vbox.visible:
 		AudioServer.set_bus_volume_db(1, linear_to_db($AudioOptions/VBoxContainer/music.value))
 		AudioServer.set_bus_volume_db(2, linear_to_db($AudioOptions/VBoxContainer/sfxslider.value))
-	else:
-		vbox.visible = true
+
+	_update_button_appearance()
+
+func _update_button_appearance():
+	if vbox.visible:
 		save_button.text = "Save"
-		
+		save_button.icon = null
+	else:
+		save_button.text = ""
+		save_button.icon = save_icon
